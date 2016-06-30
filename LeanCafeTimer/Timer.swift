2487b9e545c6d0a813100a01711da6ed08d768eb
Todd Cardoso <todd1jc@hotmail.com>
2016-06-30 14:45:03 -0700

    Rework main view

---
+++ LeanCafeTimer/Timer.swift
@@ -0,0 +1,14 @@
+//
+//  Timer.swift
+//  LeanCafeTimer
+//
+//  Created by Crystal on 2016-06-29.
+//  Copyright © 2016 TJC. All rights reserved.
+//
+
+import Foundation
+
+class Timer {
+    
+    
+}
